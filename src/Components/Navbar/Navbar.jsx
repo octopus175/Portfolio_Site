cbdade1ad04d227c8971cdf626195ffc083d0c3d
octopus175 <promolihkg@gmail.com>
2022-10-17 17:11:49 -0400

    Initialize navbar component

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+
+
+    return(
+        <div>
+            <button>Home</button>
+            <button>Projects</button>
+            <button>About</button>
+            <button>Blog</button>
+            <button>Contact me</button>
+        </div>
+    )
+}
+
+export default Navbar;
