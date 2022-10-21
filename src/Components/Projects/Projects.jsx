1f41b7889bd7c129dbd0b706e48a674ab8d4e8f7
octopus175 <promolihkg@gmail.com>
2022-10-21 18:24:01 -0400

    add projects cards

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,8 +1,32 @@
 import './Projects.style.css'
+import image from '../../Assets/Images/to-do-list-app.png'
+
 function Projects() {
+    const displayProjects = () => {
+        return (
+            <div className='project-card'>
+                <img alt='project_name' src={image} className='project-card-image'></img>
+                <div className='project-card-detail'>
+                    <p className='project-card-detail-text'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    </p>
+                    <div className='project-card-detail-techstacks'>
+
+                    </div>
+                    <div className='project-card-detail-links'>
+                        <a href='https://github.com/octopus175/to-do-list-app' target="_blank" rel="noreferrer">Github Repo</a>
+                        <a href='https://steady-tiramisu-94515d.netlify.app/' target="_blank" rel="noreferrer">Live Demo</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return(
         <section id="projects" className='projects'>
-            <h1>Porjects Page</h1>
+            <div>
+                {displayProjects()}
+            </div>
         </section>
     )
 }
