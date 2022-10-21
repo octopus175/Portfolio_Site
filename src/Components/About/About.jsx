b43ae6eeb7a4afba867496e0da37d3f82506af32
octopus175 <promolihkg@gmail.com>
2022-10-20 21:27:15 -0400

    add profile pic in about page

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,6 @@
 import './About.style.css'
 import skillImg from './skillImg';
+import profilePic from "../../Assets/Images/programmer.png";
 
 function About() {
     function skillSet(){
@@ -16,13 +17,15 @@ function About() {
     
     return(
         <section id="about" className='about'>
-            <h1>About me</h1>
-            <p className='about-paragraph'>
-            Fully committed to the philosophy of life-long learning, I'm a full stack developer with a deep passion for JavaScript, React and all things web development. 
-            The unique combination of creativity, logic, technology and never running out of new things to discover, drives my excitement and passion for web development.
-            When I'm not at my computer I like to spend my time reading, keeping fit and playing guitar.
-            </p>
-            
+            <h1 className='about-title'>About me</h1>
+            <div className='profile-text'>
+                <img alt='profile' src={profilePic} className="profile-pic"/>
+                <p className='about-paragraph'>
+                Fully committed to the philosophy of life-long learning, I'm a full stack developer with a deep passion for JavaScript, React and all things web development. 
+                The unique combination of creativity, logic, technology and never running out of new things to discover, drives my excitement and passion for web development.
+                When I'm not at my computer I like to spend my time reading, keeping fit and playing guitar.
+                </p>
+            </div>
             <div className='skills'>
                 {skillSet()}
             </div>
